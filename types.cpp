9183ef4975fb7889e30bda7ef8392f58237e89e3
connect-mr-vishal <connectmrvishal@gmail.com>
2024-08-11 10:21:47 +0530

    initial commit

---
+++ types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace  std;
+
+int main(){
+	 //int64_t x = 487384713847387422;
+	const char* x = "sdssds" ;
+	cout << x << endl;
+	cout << sizeof(x)  << endl;
+	return 0;
+
+}

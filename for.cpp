024518f06a45dea96a73072ec5d6f9f53016a6f5
connect-mr-vishal <connectmrvishal@gmail.com>
2024-08-11 22:12:10 +0530

    for loop on cpp

--- for.cpp
+++ for.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main(){
 	array<int,3> arr{1,2,3} ;
+	for(auto ele: arr){
+		cout << ele <<endl;
 	
+	}
+
+	for(auto& ele: arr){
+		cout << ele <<endl;
+	
+	}
+
 	for(int element: arr ){
 		cout << element <<endl;
 
